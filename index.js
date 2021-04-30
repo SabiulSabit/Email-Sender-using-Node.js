fbe31851d93eafaa018b24d8379a4953f4fcf721
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2021-05-01 03:41:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let transport = nodemailer.createTransport({
     service: "gmail", // replace it with your mail service name like: yahoo etc.
     auth: {
         user: "", // Your Mail Address 
-        pass: "kingsfoods2021", // Your Mail Password
+        pass: "", // Your Mail Password
     }
 })
 
 let mailOption = {
     from: "Support from XYZ <>", // Writer user mail inside <xyz@gmial.com>
-    to: "iamsabit99@gmail.com", // where you want to send the mail
+    to: "abc@gmail.com", // where you want to send the mail
     subject: "Mail Subject",
     html: ` <h1>Hello From Sabit </h1> ` // Your Custome Mail
 }
